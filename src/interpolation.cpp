17a1b25ff2a11d66cc86da62c553eeda8035f472
Arthur Goetzke-Coburn <arthur.goetzkecoburn@gmail.com>
2023-07-22 22:10:27 -0400

    changed duration variable type, gave a value to prev_value

--- src/interpolation.cpp
+++ src/interpolation.cpp
@@ -6,12 +6,12 @@ class Interpolation {
         int interpFlag = 0;
         int prev_value;
 
-        int go(int input, float duration){
+        int go(int input, unsigned long duration){
             
             if (input != prev_value){
                 interpFlag = 0;
             }
-
+            prev_value = input;
             if (interpFlag == 0){
                 myRamp.go(input, duration, LINEAR, ONCEFORWARD);
                 interpFlag = 1;
